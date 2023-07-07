5f181951f289cddf6369cbc685e1b9114de5b0ad
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-07-07 15:58:31 +0700

    feat(db): add more entities

--- src/typeorm/entities/CommentLike.ts
+++ src/typeorm/entities/CommentLike.ts
@@ -1,7 +1,5 @@
-import { CreateDateColumn, Entity, JoinColumn, ManyToMany, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { User } from "./User";
-import { Post } from "./Post";
 import { LikeAbstract } from "src/utils/abstracts/LikeAbstract";
+import { Entity, ManyToOne } from "typeorm";
 import { Comment } from "./Comment";
 
 @Entity()
